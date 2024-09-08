3b8cd86a3b05105d7372fc44d4863854fd6901fd
zydnrbrn <zidanreborn@gmail.com>
2024-09-09 06:51:39 +0700

    refactor: remove unnecessary content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -209,12 +209,6 @@ export default function Page() {
                             </h2>
                             <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                                 Want to chat? Just shoot me a dm{" "}
-                                <Link
-                                    href={DATA.contact.social.X.url}
-                                    className="text-blue-500 hover:underline"
-                                >
-                                    with a direct question on twitter
-                                </Link>{" "}
                                 and I&apos;ll respond whenever I can. I will ignore all
                                 soliciting.
                             </p>
